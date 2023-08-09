1f65ae17df1d7266af1c97dc0d27c20d4dfa3c4e
Ryan McVerry <ryan@Ryans-MacBook-Pro.local>
2023-08-09 17:03:13 -0400

    merge

--- src/stages/water.ts
+++ src/stages/water.ts
@@ -1,8 +1,9 @@
 import { Stage } from './stage'
 import { State } from '../state'
 import { Layer } from '../layer'
-import { BG_LIGHT_INDEX, BG_LIGHTER_INDEX, WHITE_INDEX } from '../colors'
-import { REFLECT_BASE, REFLECT_HORIZON, MASK } from '../reflection'
+import { Color } from '../color'
+import { BG_LIGHT_INDEX, BG_LIGHTER_INDEX, WHITE_INDEX, TRANSPARENT_INDEX } from '../colors'
+import { Reflection } from '../reflection'
 
 export class Water implements Stage {
   private waterColor: Color
@@ -14,18 +15,31 @@ export class Water implements Stage {
     this.waterReflColor = state.palette[BG_LIGHTER_INDEX]
     this.moonReflColor = state.palette[WHITE_INDEX]
 
-    const mask = getWaterMask(state.layers, state.width, state.height)
+    const mask = this.getWaterMask(state.layers, state.width, state.height)
 
+    const layer = new Layer(state.width, state.height, Reflection.NONE)
+
+    return [layer]
   }
 
-  private getWaterMask(layers: Layer[], width: number, height: number) {
-    const mask = new ArrayBuffer(width * height)
-    for (let layer of layers) {
-      if ([REFLECT_BASE, REFLECT_HORIZON, MASK].includes(layer.reflection)) {
-        origData = layer.getImageData()
-      }
+  private getWaterMask(layers: Layer[], width: number, height: number): ArrayBuffer {
+    const maskArray = new ArrayBuffer(width * height)
+    const mask = new Int8Array(maskArray)
+
+    for (let i = 0; i < mask.length; i += 1) {
+      mask[i] = 1
     }
 
-
+    for (const layer of layers) {
+      if ([Reflection.REFLECT_BASE, Reflection.REFLECT_HORIZON, Reflection.MASK].includes(layer.reflection)) {
+        const origData = layer.getImageData()
+        for (let i = 0; i < origData.data.length; i += 1) {
+          if (origData.data[i] !== TRANSPARENT_INDEX) {
+            mask[i] = 0
+          }
+        }
+      }
+    }
+    return maskArray
   }
 }
